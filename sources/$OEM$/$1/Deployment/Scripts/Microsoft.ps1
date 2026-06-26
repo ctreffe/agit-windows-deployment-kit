fd3f44bfc7b4e974c8a9f59d1c4b74f351cb89d6
Christian Treffenstaedt <treffenstaedt@psych.uni-goettingen.de>
2026-06-26 11:27:48 +0200

    Add Microsoft defaults module
    Add Microsoft-related post-install defaults.
    The module can disable OneDrive integration, Teams autostart and Copilot using policy-oriented configuration rather than removing system components.

---
+++ sources/$OEM$/$1/Deployment/Scripts/Microsoft.ps1
@@ -0,0 +1,39 @@
+﻿###############################################################################
+#
+# AGIT Deployment Kit
+#
+# Module      : Microsoft.ps1
+# Version     : 1.0.0
+# Windows     : Windows 11 Enterprise 25H2
+#
+# Author      : Christian Treffenstaedt
+# Copyright   : (c) 2026 Christian Treffenstaedt
+#
+# Description :
+# Configures Microsoft application defaults without removing system components.
+#
+###############################################################################
+
+Start-AGITLogSection 'Microsoft defaults'
+
+if ($Global:AGITConfig.DisableOneDrive) {
+    Set-AGITRegistryValue -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive' -Name 'DisableFileSyncNGSC' -Value 1 -Type DWord | Out-Null
+    Set-AGITRegistryValue -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive' -Name 'DisableFileSync' -Value 1 -Type DWord | Out-Null
+}
+
+if ($Global:AGITConfig.DisableTeamsAutostart) {
+    Remove-AGITRegistryValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'com.squirrel.Teams.Teams' | Out-Null
+    Remove-AGITRegistryValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'Teams' | Out-Null
+    Remove-AGITRegistryValue -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'MSTeams' | Out-Null
+    Write-AGITLog -Level OK -Message 'Teams auto-start entries removed if present.'
+}
+
+if ($Global:AGITConfig.DisableCopilot) {
+    Set-AGITRegistryValue -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsCopilot' -Name 'TurnOffWindowsCopilot' -Value 1 -Type DWord | Out-Null
+    Set-AGITRegistryValue -Path 'HKCU:\Software\Policies\Microsoft\Windows\WindowsCopilot' -Name 'TurnOffWindowsCopilot' -Value 1 -Type DWord | Out-Null
+}
+
+if ($Global:AGITConfig.DisableConsumerFeatures) {
+    Set-AGITRegistryValue -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent' -Name 'DisableWindowsConsumerFeatures' -Value 1 -Type DWord | Out-Null
+    Set-AGITRegistryValue -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent' -Name 'DisableConsumerAccountStateContent' -Value 1 -Type DWord | Out-Null
+}
